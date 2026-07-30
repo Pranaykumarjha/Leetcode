51a660535b2b470ea2e381ac17b75e8db4b91b8d
Pranay Kumar Jha <pranayjha952@gmail.com>
2026-07-30 10:57:29 +0530

    Time: 14 ms (92.04%), Space: 51.4 MB (74.59%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(s);
+        Arrays.sort(g);
+
+        int child=0;
+        int cookie=0;
+
+        while(child<g.length && cookie<s.length)
+        {
+            if(s[cookie]>=g[child])
+            {
+                child++;
+            }
+            cookie++;
+        }
+        return child;
+    }
+}
